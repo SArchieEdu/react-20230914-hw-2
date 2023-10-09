e0d79552335aab0c46486d9f20f2a2790eec9c53
s-archie-dev <s.artman.dev@gmail.com>
2023-10-09 20:52:12 +0300

    lesson-1/hw

---
+++ src/main.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { restaurants } from './constants/mock'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>
+      <div>{restaurants[0].name}</div>
+      <div>Menu</div>
+      <ul>
+        <li>{restaurants[0].menu[0].name}</li>
+        <li>{restaurants[0].menu[1].name}</li>
+        <li>{restaurants[0].menu[2].name}</li>
+      </ul>
+      <div>Reviews</div>
+      <ul>
+        <li>{restaurants[0].reviews[0].text}</li>
+        <li>{restaurants[0].reviews[1].text}</li>
+      </ul>
+    </div>
+    <div>
+      <div>{restaurants[1].name}</div>
+      <div>Menu</div>
+      <ul>
+        <li>{restaurants[1].menu[0].name}</li>
+        <li>{restaurants[1].menu[1].name}</li>
+      </ul>
+      <div>Reviews</div>
+      <ul>
+        <li>{restaurants[1].reviews[0].text}</li>
+        <li>{restaurants[1].reviews[1].text}</li>
+        <li>{restaurants[1].reviews[2].text}</li>
+      </ul>
+    </div>
+    <div>
+      <div>{restaurants[2].name}</div>
+      <div>Menu</div>
+      <ul>
+        <li>{restaurants[2].menu[0].name}</li>
+        <li>{restaurants[2].menu[1].name}</li>
+        <li>{restaurants[2].menu[2].name}</li>
+      </ul>
+      <div>Reviews</div>
+      <ul>
+        <li>{restaurants[2].reviews[0].text}</li>
+      </ul>
+    </div>
+    <div>
+      <div>{restaurants[3].name}</div>
+      <div>Menu</div>
+      <ul>
+        <li>{restaurants[3].menu[0].name}</li>
+        <li>{restaurants[3].menu[1].name}</li>
+      </ul>
+      <div>Reviews</div>
+      <ul>
+        <li>{restaurants[3].reviews[0].text}</li>
+        <li>{restaurants[3].reviews[1].text}</li>
+      </ul>
+    </div>
+  </React.StrictMode>,
+)
